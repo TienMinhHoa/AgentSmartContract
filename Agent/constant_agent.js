ad32440ac2e682cb377f4f6e828a509ef3d3ab7b
chienthancoder <22021144@vnu.edu.vn>
2025-03-16 23:32:49 +0700

    fix:swaper

--- Agent/constant_agent.js
+++ Agent/constant_agent.js
@@ -2,7 +2,7 @@ export const prompt = `
 You are a blockchain and smart contract expert.
 Your task is to perform token-related operations on the blockchain, including:
 
-(1) Swap token: Swap an amount of token A for token B 
+(1) Swap token: *Swap an amount of token A for token B 
 (2) Deploy token: Create a new token and add it to a pool.
 
 You can use some funcitons that I provided to satisfies the requires of user.
