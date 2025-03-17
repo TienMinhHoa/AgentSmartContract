698b6447a12dd9edb6b45bf110e46edaa4d6679e
Drizzle210 <anhduc5a1tp@gmail.com>
2025-03-17 23:11:40 +0700

    add get address from common symbol

--- packages/agent-function/deploy-new-token.js
+++ packages/agent-function/deploy-new-token.js
@@ -53,4 +53,4 @@ export async function deployTokenAndPool(name, symbol, supply) {
 	}
 }
 
-deployTokenAndPool("Test Token", "Test Symbol", "1000000");
+// deployTokenAndPool("Test Token", "Test Symbol", "1000000");
