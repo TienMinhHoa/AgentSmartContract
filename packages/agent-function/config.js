082319e00900e6aac8cea88b8c10af1e74522195
Drizzle210 <anhduc5a1tp@gmail.com>
2025-03-18 22:15:30 +0700

    add generate salt & predict token address

--- packages/agent-function/config.js
+++ packages/agent-function/config.js
@@ -7,9 +7,16 @@ dotenv.config();
  */
 function loadConfig() {
 	return {
+		WETH_ADDRESS: process.env.WETH_ADDRESS,
 		RPC_URL: process.env.RPC_URL,
 		PRIVATE_KEY: process.env.PRIVATE_KEY,
-		SIMPLE_DEPLOYER_ADDRESS: process.env.SIMPLE_DEPLOYER_ADDRESS,
+		SEPOLIA_RPC_URL: process.env.SEPOLIA_RPC_URL,
+		SEPOLIA_PRIVATE_KEY: process.env.SEPOLIA_PRIVATE_KEY,
+		UNISWAP_FACTORY: process.env.UNISWAP_FACTORY,
+		POSITION_MANAGER: process.env.POSITION_MANAGER,
+		LOCKER: process.env.LOCKER,
+		TOKEN_FACTORY: process.env.TOKEN_FACTORY,
+		CLANKER_FACTORY_V2: process.env.CLANKER_FACTORY_V2,
 	};
 }
 
--- packages/agent-function/deploy-new-token.js
+++ packages/agent-function/deploy-new-token.js
@@ -2,14 +2,15 @@ import { ethers } from 'ethers';
 
 import { logger } from '../logger/index.js';
 
-import SIMPLE_DEPLOYER_ABI from './abis/SimpleDeployerABI.json' assert { type: 'json' };
+import CLANKER_ABI from './abis/Clanker.json' assert { type: 'json' };
 import { config } from './config.js';
+import { generateSalt } from './generate-salt.js';
 
 const provider = new ethers.JsonRpcProvider(config.RPC_URL);
 const signer = new ethers.Wallet(config.PRIVATE_KEY, provider);
-const simpleDeployer = new ethers.Contract(
-	config.SIMPLE_DEPLOYER_ADDRESS,
-	SIMPLE_DEPLOYER_ABI,
+const clanker = new ethers.Contract(
+	config.CLANKER_FACTORY_V2,
+	CLANKER_ABI.abi,
 	signer,
 );
 
@@ -22,16 +23,52 @@ export async function deployTokenAndPool(name, symbol, supply) {
 		const owner = signer.address;
 		const fee = 3000;
 		const tick = 60;
+		const saltData = await generateSalt(
+			signer.address,
+			name,
+			symbol,
+			supply,
+			config.WETH_ADDRESS,
+		);
 
 		logger.info('Deploying token and pool...');
+		const poolConfig = [
+			tick,
+			config.WETH_ADDRESS,
+			10000,
+		];
 
-		const tx = await simpleDeployer.deployTokenAndPool(
+		// const allowed_pair_tx = await clanker.toggleAllowedPairedToken(
+		// 	config.WETH_ADDRESS,
+		// 	true,
+		// );
+		// await allowed_pair_tx.wait();
+		// logger.info('transaction hash:', allowed_pair_tx.hash);
+		// logger.info('Allowed pair token toggled');
+		// logger.info(`---------------------------------`);
+
+		const saltRaw = saltData.salt.startsWith("0x") ? saltData.salt : `0x${saltData.salt}`;
+		const salt = ethers.hexlify(saltRaw);
+
+		// i want to console all of params
+		logger.info(`name: ${name}`);
+		logger.info(`symbol: ${symbol}`);
+		logger.info(`initialSupply: ${initialSupply}`);
+		logger.info(`fee: ${fee}`);
+		logger.info(`salt: ${salt}`);
+		logger.info(`owner: ${owner}`);
+		logger.info(poolConfig);
+		const tx = await clanker.deployToken(
 			name,
 			symbol,
 			initialSupply,
-			owner,
 			fee,
-			tick,
+			salt,
+			owner,
+			0,
+			"",
+			"",
+			poolConfig,
 		);
 		logger.info(`Transaction hash: ${tx.hash}`);
 
@@ -40,17 +77,20 @@ export async function deployTokenAndPool(name, symbol, supply) {
 		logger.info(`✅ Token and Pool Deployed! TX Hash: ${receipt.hash}`);
 
 		// Parse the event logs to get the token and pool addresses
-		const event = receipt.logs.find((log) => log.address === config.SIMPLE_DEPLOYER_ADDRESS);
-		if (event) {
-			const parsedEvent = simpleDeployer.interface.parseLog(event);
-			logger.info(`🎉 Token Address: ${parsedEvent.args.token}`);
-			logger.info(`🎉 Pool Address: ${parsedEvent.args.pool}`);
-		}
-		return 'Successfull';
+		// const event = receipt.logs.find((log) => log.address === config.SIMPLE_DEPLOYER_ADDRESS);
+		// if (event) {
+		// 	const parsedEvent = clankerToken.interface.parseLog(event);
+		// 	logger.info(`🎉 Token Address: ${parsedEvent.args.token}`);
+		// 	logger.info(`🎉 Pool Address: ${parsedEvent.args.pool}`);
+		// }
+		// return 'Successfull';
 	} catch (error) {
-		logger.error('❌ Error deploying token and pool:', error);
-		return error;
+		console.error("❌ Deployment Error:", error);
+		if (error.data) {
+			const decodedError = clanker.interface.parseError(error.data);
+			console.error("Decoded Error:", decodedError);
+		}
 	}
 }
 
-// deployTokenAndPool("Test Token", "Test Symbol", "1000000");
+deployTokenAndPool("Test Token", "Test Symbol", "1000000");
